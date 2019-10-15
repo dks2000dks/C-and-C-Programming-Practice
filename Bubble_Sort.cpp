24cfb0dd55fd97a28737d05d392821c1e63d276d
dks2000dks <47571301+dks2000dks@users.noreply.github.com>
2019-10-15 22:30:25 +0530

    Add files via upload

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+void Input_Array(int *array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cin >> array[i];
+	}
+}
+
+void Print_Array(int *array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+void Bubble_Sort(int *a, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		int o=0;
+
+		for(int j=1;j<n;j++)
+		{
+			if(a[j-1] > a[j])
+			{
+				swap(a[j],a[j-1]);
+				o++;
+			}
+
+			else
+			{
+				o--;			
+			}
+		}
+
+		if (o + n == 0)
+		{
+			i = n;
+		}
+	}
+}
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int k[n] = {};
+	Input_Array(k,n);
+
+	Bubble_Sort(k,n);
+
+	Print_Array(k,n);
+	cout << endl;
+}
