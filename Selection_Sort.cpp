b6afb217f3d07b49ab3430496b1019ad1e9c77e2
dks2000dks <47571301+dks2000dks@users.noreply.github.com>
2019-10-16 21:24:48 +0530

    Add files via upload

---
+++ Selection_Sort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int s;
+	s = a;
+	a = b;
+	b = s;
+}
+
+
+void Print_Array(int *array, int size)
+{
+	for (int i = 0;i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+
+void Selection_Sort(int *array, int size)
+{
+	int i, j, imin;
+	
+	for(i = 0; i<size-1; i++)
+	{
+		imin = i;
+
+		for(j = i+1; j<size; j++)
+        {
+        	if(array[j] < array[imin])
+            	imin = j;
+		}
+		swap(array[i], array[imin]);
+	}
+}
+
+
+int main ()
+{
+	int n;
+	cin >> n;
+	int a[n];
+	
+	for (int i=0;i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	Selection_Sort(a,n);
+	Print_Array(a,n);
+}
